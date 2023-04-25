e619ed3eeab2c3f126c1fc0c6c215676969eb8f2
Noct2000 <oleg.kvasha.00@gmail.com>
2023-04-25 17:55:06 +0300

    [frontend]: add eslint

--- frontend/src/app/page/questions/questions.component.ts
+++ frontend/src/app/page/questions/questions.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
   styleUrls: ['./questions.component.scss']
 })
-export class QuestionsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class QuestionsComponent {
 
 }
