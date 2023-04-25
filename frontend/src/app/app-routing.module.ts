340642c6bc0e762289d8a031e9832fcf0a6400a4
Noct2000 <oleg.kvasha.00@gmail.com>
2023-04-25 17:10:08 +0300

    [frontend]: rename pages to page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/questions' },
-  { path: 'questions', loadChildren: () => import('./pages/questions/questions.module').then(m => m.QuestionsModule) },
-  { path: "curators", loadChildren: () => import('./pages/curators/curators.module').then(m => m.CuratorsModule) },
-  { path: "telegram-users", loadChildren: () => import('./pages/telegram-users/telegram-users.module').then(m => m.TelegramUsersModule) },
-  { path: "send-message", loadChildren: () => import('./pages/send-message/send-message.module').then(m => m.SendMessageModule) },
+  { path: 'questions', loadChildren: () => import('./page/questions/questions.module').then(m => m.QuestionsModule) },
+  { path: "curators", loadChildren: () => import('./page/curators/curators.module').then(m => m.CuratorsModule) },
+  { path: "telegram-users", loadChildren: () => import('./page/telegram-users/telegram-users.module').then(m => m.TelegramUsersModule) },
+  { path: "send-message", loadChildren: () => import('./page/send-message/send-message.module').then(m => m.SendMessageModule) },
 ];
 
 @NgModule({
